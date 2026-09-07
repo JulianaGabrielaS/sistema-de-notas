3ce658041aca2c25cc9ccd363d2728e022f96e38
Juliana Gabriela <julianagsantos1@outlook.com>
2026-09-07 12:57:35 -0300

    Adiciona validação das notas

--- sistema_notas.py
+++ sistema_notas.py
@@ -5,15 +5,18 @@ nota1 = float(input('Digite a primeira nota: '))
 nota2 = float(input('Digite a segunda nota: '))
 nota3 = float(input('Digite a terceira nota: '))
 
-media = (nota1 + nota2 + nota3) / 3
+if 0 <= nota1 <= 10 and 0 <= nota2 <= 10 and 0 <= nota3 <= 10:
+    media = (nota1 + nota2 + nota3) / 3
 
-print('=== RESULTADO ===')
-print('Aluno: ', nome_aluno)
-print('Média: ', media)
+    print('=== RESULTADO ===')
+    print('Aluno: ', nome_aluno)
+    print('Média: ', media)
 
-if media >= 6:
-    print('Situação: Aprovado.')
-elif media >= 5:
-    print('Situação: Recuperação.')
+    if media >= 6:
+        print('Situação: Aprovado.')
+    elif media >= 5:
+        print('Situação: Recuperação.')
+    else:
+        print('Situação: Reprovado.')
 else:
-    print('Situação: Reprovado.')
+    print('Nota inválida.')
