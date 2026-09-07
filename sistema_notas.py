3c0c980ab931053b69aab87dc48c41e0a3a5ddd9
Juliana Gabriela <julianagsantos1@outlook.com>
2026-09-07 12:30:15 -0300

    Adiciona sistema de notas

---
+++ sistema_notas.py
@@ -0,0 +1,19 @@
+print('=== SISTEMA DE NOTAS ===')
+
+nome_aluno = input('Nome do Aluno: ')
+nota1 = float(input('Digite a primeira nota: '))
+nota2 = float(input('Digite a segunda nota: '))
+nota3 = float(input('Digite a terceira nota: '))
+
+media = (nota1 + nota2 + nota3) / 3
+
+print('=== RESULTADO ===')
+print('Aluno: ', nome_aluno)
+print('Média: ', media)
+
+if media >= 6:
+    print('Situação: Aprovado.')
+elif media >= 5:
+    print('Situação: Recuperação.')
+else:
+    print('Situação: Reprovado.')
